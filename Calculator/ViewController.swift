7954ac3f98605ff421d819fb1b942639b4b9e7c3
Kelvin Zhang <zhangxp1998@gmail.com>
2017-04-23 21:00:09 -0400

    Add digit buttons

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,17 +9,61 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    //    override func viewDidLoad() {
+    //        super.viewDidLoad()
+    //        // Do any additional setup after loading the view, typically from a nib.
+    //    }
+    //
+    //    override func didReceiveMemoryWarning() {
+    //        super.didReceiveMemoryWarning()
+    //        // Dispose of any resources that can be recreated.
+    //    }
+    
+    var userIsTyping = false;
+    var displayValue: Double {
+        get{
+            return Double(display.text!)!;
+        }
+        set{
+            display.text = String(newValue);
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func touchDigit(_ sender: UIButton)
+    {
+        let digit = sender.currentTitle!;
+        let textCurrentlyInDisplay = display.text!;
+        
+        if !userIsTyping
+        {
+            display.text = digit;
+            userIsTyping = true;
+        }else
+        {
+            display!.text = textCurrentlyInDisplay + digit;
+        }
+        
+        
     }
-
-
+    @IBAction func performOperation(_ sender: UIButton)
+    {
+        userIsTyping = false;
+        if let mathSymbol = sender.currentTitle
+        {
+            switch mathSymbol
+            {
+            case "π":
+                displayValue = Double.pi;
+            case "√":
+                displayValue = sqrt(displayValue);
+            default:
+                break;
+            }
+        }
+    }
+    
+    @IBOutlet weak var display: UILabel!
+    
 }
 
